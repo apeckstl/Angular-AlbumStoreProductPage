42b75c13ac5af4b2d1217723be0d7963ac275761
Avery Peck <ap058641@abcsupply.com>
2021-03-04 17:16:40 -0600

    Adding the Tracklisting Component section

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
-import { Album } from '../album';
+import { AlbumInfo } from '../album-info';
 
 @Component({
   selector: 'app-product-description',
@@ -9,7 +9,7 @@ import { Album } from '../album';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo: Album;
+  albumInfo: AlbumInfo;
 
   constructor(private _productService: ProductService) { }
 
---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+
+import { AlbumInfo } from '../album-info';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  albumInfo: AlbumInfo;
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+  }
+
+}
