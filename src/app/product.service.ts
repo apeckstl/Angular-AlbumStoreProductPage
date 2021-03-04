6af1e3eec22250e554fc433f67d8a9fae055346e
Avery Peck <ap058641@abcsupply.com>
2021-03-04 17:30:15 -0600

    Displaying a List of Products section

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,16 +4,22 @@ import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
 import { AlbumInfo } from './album-info';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) : Observable<AlbumInfo> {
+  getAlbum(id: number): Observable<AlbumInfo> {
     return this._http.get(this._albumUrl).map(response => <AlbumInfo>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 
 }
